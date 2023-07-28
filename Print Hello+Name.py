848a9145a420ab75dbfbb2c112973c172f162bd7
Parisinsa <125097498+Parisinsa@users.noreply.github.com>
2023-07-28 14:38:43 +0200

    Add files via upload
    The program code prompt the user to input his details then the program greet that person name

---
+++ Print Hello+Name.py
@@ -0,0 +1,7 @@
+#Enter personal data
+Name=input(str("Enter your name: "))
+Surname=input(str("Enter your surname: "))
+#Code will great and display your information
+print("Hello")
+print(Name)
+print(Surname)
